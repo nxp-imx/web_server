9ba4f1ccc95ca7c6072b43beabcf786fc2cf95cd
Steven Yang <zhao.yang_1@nxp.com>
2025-05-29 17:37:39 +0800

    feat:Add persistent menu bar for switching to other webpages
    Signed-off-by: Steven Yang <zhao.yang_1@nxp.com>

--- php/bluetooth.php
+++ php/bluetooth.php
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
     exec('sh /www/pages/web_server/sh/bt_init.sh'); //Configure the Bluetooth module on OrangeBox
-    // exec('bluetoothctl show',$output);   //Show the infomation of Bluetooth on OrangeBox
+    // exec('bluetoothctl show',$output);   //Show the information of Bluetooth on OrangeBox
     exec('bluetoothctl power on');  //Power on the Bluetooth module
     exec('bluetoothctl discoverable on');   //Ensure the OrangeBox could be found by other devices
     exec('bluetoothctl pairable on');
