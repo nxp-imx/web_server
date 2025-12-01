267d5a16d5547ada5e8dabb36009413b8c43c6eb
Steven Yang <zhao.yang_1@nxp.com>
2025-12-01 17:05:37 +0800

    [OB-424]BT : Fix the wrong display connected status
    The connected status in the webUI is ok, but the two device are
    not connected successfully at last.Add the delay to check the status.
    
    Signed-off-by: Steven Yang <zhao.yang_1@nxp.com>

--- sh/bt_status.sh
+++ sh/bt_status.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #three status：1.no paired device；2.has paired device but not connect；3.device connected
 devnumber=$(bluetoothctl devices Paired | awk -F ' ' '{print $2}') 
-
+sleep 5
 if [ ! -n "$devnumber" ]; then
     echo "status 1"
     echo "There is no paired device"
